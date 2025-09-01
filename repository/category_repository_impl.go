fb8505547914c92792757f7e0c8ad32937c07152
NazalPrastya <nazalprastya@gmail.com>
2025-09-01 15:36:27 +0700

    impl repository

--- repository/category_repository_impl.go
+++ repository/category_repository_impl.go
@@ -1,9 +1,11 @@
 package repository
 
 import (
+	"belajar-go-api/helper"
 	"belajar-go-api/model/domain"
 	"context"
 	"database/sql"
+	"errors"
 )
 
 type CategoryRepositoryImpl struct {
@@ -14,31 +16,58 @@ type CategoryRepositoryImpl struct {
 func (c CategoryRepositoryImpl) Save(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
 	SQL := "insert into category(name) values (?)"
 	result, err := tx.ExecContext(ctx, SQL, category.Name)
-	if err != nil {
-		panic(err)
-	}
+	helper.PanicIfError(err)
 
 	result.LastInsertId()
 	id, err := result.LastInsertId()
-	if err != nil {
-		panic(err)
-	}
+	helper.PanicIfError(err)
 	category.Id = int(id)
 	return category
 }
 
 func (c CategoryRepositoryImpl) Update(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
-	panic("unimplemented")
+	SQL := "update category set name = ? where id = ?"
+	_, err := tx.ExecContext(ctx, SQL, category.Name, category.Id)
+	helper.PanicIfError(err)
+
+	return category
 }
 
 func (c CategoryRepositoryImpl) Delete(ctx context.Context, tx *sql.Tx, category domain.Category) {
-	panic("unimplemented")
+ SQL:= "delete from category where id = ?"
+ _, err := tx.ExecContext(ctx, SQL, category.Id)
+ helper.PanicIfError(err)
+ 
 }
 
-func (c CategoryRepositoryImpl) FindById(ctx context.Context, tx *sql.Tx, categoryId int) domain.Category {
-	panic("unimplemented")
+func (repository *CategoryRepositoryImpl) FindById(ctx context.Context, tx *sql.Tx, categoryId int) (domain.Category, error) {
+	SQL := "select id, name from category where id = ?"
+	rows, err := tx.QueryContext(ctx, SQL, categoryId)
+	helper.PanicIfError(err)
+	defer rows.Close()
+
+	category := domain.Category{}
+	if rows.Next() {
+		err := rows.Scan(&category.Id, &category.Name)
+		helper.PanicIfError(err)
+		return category, nil
+	} else {
+		return category, errors.New("category is not found")
+	}
 }
 
-func (c CategoryRepositoryImpl) FindAll(ctx context.Context, tx *sql.Tx, category domain.Category) []domain.Category {
-	panic("unimplemented")
+func (repository *CategoryRepositoryImpl) FindAll(ctx context.Context, tx *sql.Tx) []domain.Category {
+	SQL := "select id, name from category"
+	rows, err := tx.QueryContext(ctx, SQL)
+	helper.PanicIfError(err)
+	defer rows.Close()
+
+	var categories []domain.Category
+	for rows.Next() {
+		category := domain.Category{}
+		err := rows.Scan(&category.Id, &category.Name)
+		helper.PanicIfError(err)
+		categories = append(categories, category)
+	}
+	return categories
 }
